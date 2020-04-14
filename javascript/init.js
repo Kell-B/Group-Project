d1ab86e762d077a0731473184b59a5cacd390e30
Austin Burnett <tinz1320@gmail.com>
2020-04-13 20:10:43 -0600

    Added product page, changed navbar.

--- javascript/init.js
+++ javascript/init.js
@@ -6,3 +6,30 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+$('.dropdown-trigger').dropdown({
+    coverTrigger: false,
+    hover: true,
+    constrainWidth: false,
+    inDuration: 350,
+});
+
+var modal = document.getElementById('modal');
+
+var btn = document.getElementById("modbtn");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
